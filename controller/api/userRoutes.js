56263bb88d39e4ebd20267427acdbf7253b19561
Drew Selden <seldend1@gmail.com>
2021-09-13 17:18:38 -0400

    post and user api routes

---
+++ controller/api/userRoutes.js
@@ -0,0 +1,128 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.post('/', async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.post('/login', async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { email: req.body.email } });
+
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      
+      res.json({ user: userData, message: 'You are now logged in!' });
+    });
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+router.get('/', async (req, res) => {
+  try {
+    const usersData = await User.findAll({
+      attributes: { exclude: ['password'] }
+    });
+    
+    res.status(200).json(usersData);
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/:name', async (req, res) => {
+  try {
+    const usersData = await User.findOne({ 
+      where: { name: req.params.name },
+      attributes: { exclude: ['password'] }
+    });
+    
+    res.status(200).json(usersData);
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const userData = await User.destroy({
+      where: {
+        id: req.params.id
+      },
+    });
+
+    if (!userData) {
+      res.status(404).json({ message: 'No user found with this id!' });
+      return;
+    }
+
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    const userData = await User.update(req.body, {
+      individualHooks: true,
+      where: {
+        id: req.params.id
+      },
+    });
+
+    if (!userData) {
+      res.status(404).json({ message: 'No user found with this id!' });
+      return;
+    }
+
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
