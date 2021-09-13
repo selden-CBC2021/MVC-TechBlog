140205b2c470112661b38c348138c5657fa41582
Drew Selden <seldend1@gmail.com>
2021-09-13 17:48:05 -0400

    withAuth function and helper function for displaying date/time format

---
+++ models/index.js
@@ -0,0 +1,22 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
+Post.hasOne(Post, {
+  foreignKey: 'post_id'
+});
+
+
+module.exports = { User, Post, Comment };
